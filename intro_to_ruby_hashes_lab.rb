c29ce3ead57a0c33b6282b84ae4849438c000c62
Annie Truex <artruex@gmail.com>
2019-07-28 00:15:21 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -52,7 +52,8 @@ def monopoly_with_fourth_tier
 	      b_and_o_railroad: {
 	        "mortgage_value" => "$400"
 	      },
-	      shortline: {}
+	      shortline: 
+	      "mortgage_value" => "$800"
 	    },
 	    rent_in_dollars: {one_piece_owned: 25,
 	      two_pieces_owned: 50,
