f76f009f02c79ba935395ed08350d544335347b8
Annie Truex <artruex@gmail.com>
2019-07-28 00:12:06 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -43,7 +43,7 @@ end
 def monopoly_with_fourth_tier
 	monopoly = {
 	  railroads: { pieces: 4, 
-	    names: {reading_railroad: {}, 
+	    names: {reading_railroad: {"morgage value" => "$100"}, 
 	      pennsylvania_railroad: {},
 	      b_and_o_railroad: {},
 	      shortline: {}
